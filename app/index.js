d80b57e853100de4e1196d5d52d2c430aa137878
Archana Sriram <archana@wheelhousehq.com>
2015-11-12 09:42:34 -0700

    challenge complete

--- app/index.js
+++ app/index.js
@@ -1,14 +1,48 @@
-import Router from './router'
+var fs = require('fs');
+var request = require('request');
+var JsonApiService = require('../lib/json_api_service');
 
-class App {
-  constructor() {
-    const router = new Router()
-    this.router = router
+var TOKEN = require('../config')['TOKEN'];
+
+function App() {
+  
+  function home(req, res) {
+    fs.readFile(__dirname + '/layout/index.html', 'utf8', function(err, text){
+      res.send(text);
+    });
   }
 
-  init() {
-    this.router.history.start()
-  }
-}
+  function search(req, res) {
+    var query = req.query.query;
+    var queryString = '?filter[contains]=' + query;
 
-export default new App
+    var options = {
+      url: 'https://ivy.vaynermedia.com/api/search' + queryString,
+      headers: {
+        'Authorization': 'Token ' + TOKEN
+      }
+    };
+
+    var callback = function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var info = JSON.parse(body);
+
+        jsonPayload = JsonApiService.process(info);
+
+        res.send(jsonPayload);
+      }
+      else {
+        console.log('response statuscode:', response.statusCode);
+      }
+    };
+
+    request(options, callback);
+  }
+
+  return {
+    home: home,
+    search: search
+  };
+};
+
+module.exports = App();
--- index.js
+++
@@ -1,4 +0,0 @@
-import app from './app'
-
-window.app = app
-app.init()
